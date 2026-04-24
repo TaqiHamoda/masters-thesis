added sparse visualization and fixed gravity vector calculation

src/photogrammetry.py
@@ -62,20 +62,20 @@ class Photogrammetry:
 
         # Source: https://github.com/colmap/colmap/issues/2976#issuecomment-3930305589
         with pycolmap.Database.open(self.database_path) as colmap_db:
-            gravity = np.array((0.0, 0.0, 1.0)).reshape(3, 1)  # In the NED frame
 
             position_covariance = np.diag(np.power(pos_std, 2))
             for image in colmap_db.read_all_images():
                 ts = int(image.name.replace(".jpg", ''))
                 pose = self.dataset.images[ts].pose
-                position = pose.get_position().reshape(3, 1)
+                position = pose.get_position()
+                gravity = pose.get_rotation_matrix().T @ np.array((0, 0, 1))
 
                 # Coordinate system: Cartesian (X,Y,Z coords, not Lat/Lon)
                 colmap_db.write_pose_prior(
                     pycolmap.PosePrior(
                         corr_data_id=image.data_id,  # Link the prior to the specific image's data identifier
-                        position=position,
-                        gravity=gravity,
+                        position=position.reshape(3, 1),
+                        gravity=gravity.reshape(3, 1),
                         position_covariance=position_covariance,
                         coordinate_system=pycolmap.PosePriorCoordinateSystem.CARTESIAN
                     )

src/registration/utils.py
@@ -5,11 +5,14 @@ from typing import Tuple, List, Dict
 from ..dataset import Dataset, Pose
 
 
-def interpolate(pose_timestamps: List[int], refined_timestamps: List[int], refined_poses: Dict[int, pycolmap.Rigid3d]) -> Dict[int, Pose]:
+def interpolate(poses: Dict[int, Pose], refined_poses: Dict[int, pycolmap.Rigid3d]) -> Dict[int, Pose]:
     # Source: https://deepwiki.com/colmap/pycolmap/4.4-geometric-transformations#rigid3d
     # Source: https://colmap.github.io/pycolmap/pycolmap.html#pycolmap.Rigid3d
 
     inter_poses = {}
+
+    pose_timestamps = np.array(sorted(poses.keys()))
+    refined_timestamps = np.array(sorted(refined_poses.keys()))
 
     n = len(refined_timestamps)
     for ts in pose_timestamps:
@@ -55,10 +58,8 @@ def interpolate_poses(dataset: Dataset, reconstruction: pycolmap.Reconstruction)
         timestamp = int(image.name.replace(".jpg", ''))
         refined_poses[timestamp] = image.cam_from_world()
 
-    refined_timestamps = np.array(sorted(refined_poses.keys()))
-
-    camera_poses = interpolate(dataset.images.keys(), refined_timestamps, refined_poses)
-    sonar_poses = interpolate(dataset.sonar.keys(), refined_timestamps, refined_poses)
+    camera_poses = interpolate(dataset.images, refined_poses)
+    sonar_poses = interpolate(dataset.sonar, refined_poses)
 
     # Correct the extrinsics for sonar pose (colmap is wrt camera frame)
     for ts in list(sonar_poses.keys()):